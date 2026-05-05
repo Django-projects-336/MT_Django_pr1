6bb573f53877a258d6a0d322579128a8d8f8eb9b
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-05 15:05:25 +0530

    template variables

--- Ind_AP/views.py
+++ Ind_AP/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def chittoor(request) :
+        user_name = "Tony"
+        context = {"name": user_name}
         response = "<h1> Welcome to Chittoor</h1>"
-        return render(request, "chittoor.html")
+        return render(request, "chittoor.html", context)
 
 
 def Kadapa(request) :
